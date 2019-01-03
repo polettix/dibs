04b294c0ec08789ac91962a06cbd41fa0a63a5ed
Flavio Poletti <flavio@polettix.it>
2019-01-03 08:31:04 +0100

    Remove cruft

--- lib/Dibs/Role/PackStroke.pm
+++
@@ -1,8 +0,0 @@
-package Dibs::Role::PackStroke;
-use 5.024;
-use Moo::Role;
-use experimental qw< postderef signatures >;
-no warnings qw< experimental::postderef experimental::signatures >;
-
-
-1;
