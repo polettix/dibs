b1642c45089c77c1033aa8366f20658289b24756
Flavio Poletti <flavio@polettix.it>
2019-03-08 22:48:05 +0100

    Add two variables functions

--- lib/Dibs/Variables.pm
+++ lib/Dibs/Variables.pm
@@ -6,17 +6,19 @@ use English qw< -no_match_vars >;
 use experimental qw< postderef signatures >;
 no warnings qw< experimental::postderef experimental::signatures >;
 
-sub dvf_array_flatten ($o, $x)  { return $x->@*                       }
-sub dvf_array_wrap ($o, @x)     { return [@x]                         }
-sub dvf_dibs_id ($o)            { return $o->{run_variables}{DIBS_ID} }
-sub dvf_env ($o, @x)            { return @ENV{@x}                     }
-sub dvf_get_runvar ($o, $key)   { return $o->{run_variables}{$key}    }
-sub dvf_get_var ($o, $key)      { return $o->{named_variables}{$key}  }
-sub dvf_join ($o, $sep, @x)     { return join $sep, @x                }
-sub dvf_passthrough ($o, @x)    { return @x                           }
-sub dvf_set_runvar ($o, $k, $v) { $o->{run_variables}{$k} = $v        }
-sub dvf_set_var ($o, $k, $v)    { $o->{named_variables}{$k} = $v      }
-sub dvf_sprintf ($o, $fmt, @x)  { return sprintf $fmt, @x             }
+sub dvf_array_flatten ($o, $x)     { return $x->@*                       }
+sub dvf_array_wrap ($o, @x)        { return [@x]                         }
+sub dvf_dibs_id ($o)               { return $o->{run_variables}{DIBS_ID} }
+sub dvf_empty_default ($o, $x, $y) { return length($x) ? $x : $y         }
+sub dvf_env ($o, @x)               { return @ENV{@x}                     }
+sub dvf_get_runvar ($o, $key)      { return $o->{run_variables}{$key}    }
+sub dvf_get_var ($o, $key)         { return $o->{named_variables}{$key}  }
+sub dvf_join ($o, $sep, @x)        { return join $sep, @x                }
+sub dvf_passthrough ($o, @x)       { return @x                           }
+sub dvf_set_runvar ($o, $k, $v)    { $o->{run_variables}{$k} = $v        }
+sub dvf_set_var ($o, $k, $v)       { $o->{named_variables}{$k} = $v      }
+sub dvf_sprintf ($o, $fmt, @x)     { return sprintf $fmt, @x             }
+sub dvf_undef_default ($o, $x, $y) { return $x // $y                     }
 
 # time-specific functions
 sub dvf_dibs_date ($o)  { return $o->{run_variables}{DIBS_DATE}          }
